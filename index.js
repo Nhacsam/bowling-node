13e1cf1dab81897e49b4902b5946fe930eed469e
Nicolas Djambazian <nicolas@djambazian.fr>
2021-02-26 17:39:32 +0100

    My Vo result

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-
 class Bowling {
+  constructor() {
+    this.pins = 0;
+    this.rollCount = 0;
+    this.previousRoll = 0;
+    this.isSpare = false;
+  }
 
+  roll(pins) {
+    console.log(this.rollCount, pins);
+    console.log("isSpare", this.isSpare);
+    console.log("previous roll", this.previousRoll);
+
+    if (this.isSpare) {
+      this.pins += pins;
+      console.log("set isSpare to false");
+      this.isSpare = false;
+    }
+
+    if (this.rollCount % 2 === 1) {
+      // even
+      console.log(this.previousRoll, pins);
+      if (this.previousRoll + pins === 10) {
+        console.log("set isSpare to true");
+        this.isSpare = true;
+      }
+    }
+
+    this.pins += pins;
+
+    this.rollCount++;
+    this.previousRoll = pins;
+  }
+
+  getScore() {
+    return this.pins;
+  }
 }
 
 module.exports = Bowling;
--- test.js
+++ test.js
@@ -1,8 +1,38 @@
-const Bowling = require('./index');
+const Bowling = require("./index");
+describe("Bowling", () => {
+  it("should compute a very bad game", () => {
+    const bowling = new Bowling();
+    for (let i = 0; i < 20; i++) {
+      bowling.roll(0);
+    }
+    expect(bowling.getScore()).toBe(0);
+  });
 
-describe('Bowling', () => {
+  it("should compute a simple game", () => {
+    const bowling = new Bowling();
 
-  it('should be defined', () => {
-    expect(new Bowling()).toBeDefined();
+    bowling.roll(3);
+    bowling.roll(2);
+    bowling.roll(7);
+    bowling.roll(1);
+
+    for (let i = 0; i < 20; i++) {
+      bowling.roll(0);
+    }
+    expect(bowling.getScore()).toBe(13);
+  });
+
+  it.only("should compute a simple game", () => {
+    const bowling = new Bowling();
+
+    bowling.roll(7);
+    bowling.roll(3);
+    bowling.roll(7);
+    bowling.roll(1);
+
+    // for (let i = 0; i < 20; i++) {
+    //   bowling.roll(0);
+    // }
+    expect(bowling.getScore()).toBe(25);
   });
 });
