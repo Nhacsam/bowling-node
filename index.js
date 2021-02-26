c06cc79a86a81d2c46c7af4e3f8ee92bf1f9ba58
Nicolas Djambazian <nicolas@djambazian.fr>
2021-02-26 17:53:25 +0100

    Solution 2

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-
 class Bowling {
+  constructor() {
+    this.rolls = [];
+  }
 
+  roll(pins) {
+    this.rolls.push(pins);
+  }
+
+  getScore() {
+    let score = 0;
+
+    let isEndOfFrame = false;
+
+    for (let i = 0; i < this.rolls.length; i++) {
+      const pins = this.rolls[i];
+
+      score += pins;
+
+      const isSpare = isEndOfFrame && i > 0 && pins + this.rolls[i - 1] === 10;
+      if (isSpare) {
+        score += this.rolls[i + 1];
+      }
+
+      const isStrike = !isEndOfFrame && pins === 10;
+      if (isStrike) {
+        score += this.rolls[i + 1] + this.rolls[i + 2];
+        isEndOfFrame = true;
+      }
+
+      isEndOfFrame = !isEndOfFrame;
+    }
+
+    return score;
+  }
 }
 
 module.exports = Bowling;
--- test.js
+++ test.js
@@ -1,8 +1,51 @@
-const Bowling = require('./index');
+const Bowling = require("./index");
+describe("Bowling", () => {
+  it("should compute a very bad game", () => {
+    const bowling = new Bowling();
+    for (let i = 0; i < 20; i++) {
+      bowling.roll(0);
+    }
+    expect(bowling.getScore()).toBe(0);
+  });
 
-describe('Bowling', () => {
+  it("should compute a simple game", () => {
+    const bowling = new Bowling();
 
-  it('should be defined', () => {
-    expect(new Bowling()).toBeDefined();
+    bowling.roll(3);
+    bowling.roll(2);
+    bowling.roll(7);
+    bowling.roll(1);
+
+    for (let i = 0; i < 20; i++) {
+      bowling.roll(0);
+    }
+    expect(bowling.getScore()).toBe(13);
+  });
+
+  it("should compute a spare", () => {
+    const bowling = new Bowling();
+
+    bowling.roll(7);
+    bowling.roll(3);
+    bowling.roll(7);
+    bowling.roll(1);
+
+    for (let i = 0; i < 20; i++) {
+      bowling.roll(0);
+    }
+    expect(bowling.getScore()).toBe(25);
+  });
+
+  it("should compute a strike", () => {
+    const bowling = new Bowling();
+
+    bowling.roll(10);
+    bowling.roll(7);
+    bowling.roll(1);
+
+    for (let i = 0; i < 20; i++) {
+      bowling.roll(0);
+    }
+    expect(bowling.getScore()).toBe(26);
   });
 });
