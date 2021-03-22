bda5101ae56b64083812bf0ed3765b6407ddfdd2
Nicolas Djambazian <nicolas@djambazian.fr>
2021-03-22 15:22:57 +0100

    End of the exo

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-
 class Bowling {
+  constructor() {
+    this.allRolls = [];
+  }
+  roll(pins) {
+    this.allRolls.push(pins);
+  }
 
+  getScore() {
+    let score = 0;
+
+    console.log(this.allRolls);
+
+    for (let i = 0; i < this.allRolls.length / 2; i++) {
+      const pinsRoll1 = this.allRolls[2 * i];
+      const pinsRoll2 = this.allRolls[2 * i + 1];
+
+      score += pinsRoll1 + pinsRoll2;
+      if (pinsRoll1 + pinsRoll2 === 10) {
+        score = score + this.allRolls[2 * (i + 1)];
+      }
+    }
+
+    return score;
+  }
 }
 
 module.exports = Bowling;
--- test.js
+++ test.js
@@ -1,8 +1,42 @@
-const Bowling = require('./index');
+const Bowling = require("./index");
 
-describe('Bowling', () => {
+describe("Bowling", () => {
+  it("should compute the score for a very bad game", () => {
+    const bowling = new Bowling();
 
-  it('should be defined', () => {
-    expect(new Bowling()).toBeDefined();
+    for (let i = 0; i < 20; i++) {
+      bowling.roll(0);
+    }
+
+    expect(bowling.getScore()).toBe(0);
+  });
+
+  it("should compute the score for an other game", () => {
+    const bowling = new Bowling();
+
+    bowling.roll(2);
+    bowling.roll(3);
+    bowling.roll(4);
+    bowling.roll(2);
+    for (let i = 0; i < 16; i++) {
+      bowling.roll(0);
+    }
+
+    expect(bowling.getScore()).toBe(11);
+  });
+  it("should compute the score for a spare", () => {
+    const bowling = new Bowling();
+
+    bowling.roll(2);
+    bowling.roll(8);
+
+    bowling.roll(4);
+    bowling.roll(2);
+
+    for (let i = 0; i < 16; i++) {
+      bowling.roll(0);
+    }
+
+    expect(bowling.getScore()).toBe(20);
   });
 });
