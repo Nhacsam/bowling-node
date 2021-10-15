2a2211e5338ce8a3c9a2b5187d16d0335526fb98
Nicolas Djambazian <nicolas@djambazian.fr>
2021-10-15 15:51:06 +0200

    End of interview

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-
 class Bowling {
+  constructor() {
+    this._rolls = [];
+  }
 
+  roll(pins) {
+    this._rolls.push(pins);
+  }
+
+  isSpare(rollNumber) {
+    if (rollNumber < 2) {
+      return false;
+    }
+    const first = this._rolls[rollNumber - 2];
+    const last = this._rolls[rollNumber - 1];
+    const spare = first + last;
+    return spare === 10;
+  }
+
+  getScore() {
+    let score = 0;
+    for (let rollNumber in this._rolls) {
+      const pins = this._rolls[rollNumber];
+
+      const spare = this.isSpare(rollNumber);
+
+      if (spare) {
+        score += pins;
+      }
+
+      score += pins;
+    }
+    return score;
+  }
 }
 
 module.exports = Bowling;
--- test.js
+++ test.js
@@ -1,8 +1,55 @@
-const Bowling = require('./index');
+const Bowling = require("./index");
+describe("Bowling", () => {
+  it("should compute the score for a bad game", () => {
+    const bowling = new Bowling();
+    for (let i = 0; i < 20; i++) {
+      bowling.roll(0);
+    }
+    const score = bowling.getScore();
+    expect(score).toBe(0);
+  });
 
-describe('Bowling', () => {
+  it("should compute the score be result of array", () => {
+    const bowling = new Bowling();
+    const rolls = [1, 4, 3, 2];
 
-  it('should be defined', () => {
-    expect(new Bowling()).toBeDefined();
+    rolls.forEach((roll) => {
+      bowling.roll(roll);
+    });
+
+    for (let i = 0; i < 20 - rolls.length; i++) {
+      bowling.roll(0);
+    }
+    const score = bowling.getScore();
+    expect(score).toBe(10);
+  });
+
+  it("should compute the score for a spare", () => {
+    const bowling = new Bowling();
+    const rolls = [4, 6, 3, 2];
+
+    rolls.forEach((roll) => {
+      bowling.roll(roll);
+    });
+
+    for (let i = 0; i < 20 - rolls.length; i++) {
+      bowling.roll(0);
+    }
+    const score = bowling.getScore();
+    expect(score).toBe(18);
+  });
+  it("should compute the score for not a spare", () => {
+    const bowling = new Bowling();
+    const rolls = [1, 5, 5, 3];
+
+    rolls.forEach((roll) => {
+      bowling.roll(roll);
+    });
+
+    for (let i = 0; i < 20 - rolls.length; i++) {
+      bowling.roll(0);
+    }
+    const score = bowling.getScore();
+    expect(score).toBe(14);
   });
 });
