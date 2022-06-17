8e4babd36181432a3c4dbf6b46d12afc0b360d3e
Nicolas Djambazian <nicolas@djambazian.fr>
2022-06-17 15:07:24 +0200

    End of the interview

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-
 class Bowling {
+
+    constructor() {
+        this.currentScore = 0;
+
+        this.frameScore = 0;
+        this.rollCount = 0;
+        this.wasASpare = false;
+    }
+
+    roll(pins) {
+
+        this.currentScore += pins;
+
+        if (this.wasASpare) {
+            this.currentScore += pins;
+            this.wasASpare = false;
+        }
+
+
+        if (this.rollCount % 2 == 0) {
+            this.frameScore = 0;
+        }
+        this.frameScore += pins;
+        this.rollCount++;
+
+        if (this.frameScore === 10) {
+            this.wasASpare = true;
+        }
+
+    }
+
+    getScore() {
+        return this.currentScore;
+    }
 
 }
 
--- test.js
+++ test.js
@@ -2,7 +2,47 @@ const Bowling = require('./index');
 
 describe('Bowling', () => {
 
-  it('should be defined', () => {
-    expect(new Bowling()).toBeDefined();
-  });
+    it('should return 0 if no pins have fallen', () => {
+        const bowling = new Bowling();
+
+        for (let i = 0; i < 20; i++) {
+            bowling.roll(0);
+        }
+
+        expect(bowling.getScore()).toBe(0);
+    });
+
+    it('should works for a simple game', () => {
+        const bowling = new Bowling();
+
+        bowling.roll(3);
+        bowling.roll(2);
+        bowling.roll(4);
+        bowling.roll(2);
+        for (let i = 0; i < 16; i++) {
+            bowling.roll(0);
+        }
+
+        expect(bowling.getScore()).toBe(11);
+
+    });
+
+
+    it('should works for a game with a spare', () => {
+        const bowling = new Bowling();
+
+        bowling.roll(6);
+        bowling.roll(4);
+
+        bowling.roll(4);
+        bowling.roll(2);
+
+        for (let i = 0; i < 16; i++) {
+            bowling.roll(0);
+        }
+
+        expect(bowling.getScore()).toBe(20);
+
+    });
+
 });
